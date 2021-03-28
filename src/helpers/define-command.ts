21b2ceef281c7acef0447e870646e7ce88e6f0eb
Giovanni Cavallo <giovannicavallo95@gmail.com>
2021-03-28 14:09:10 +0200

    add optional async handler

--- src/helpers/define-command.ts
+++ src/helpers/define-command.ts
@@ -3,7 +3,10 @@ import ResHelper from "./res-helper";
 
 export type BotCommand = Omit<ApplicationCommand, "id" | "application_id"> & {
   scope: string[];
-  handler: (req: Interaction, res: ResHelper) => InteractionResponse;
+  handler: (
+    req: Interaction,
+    res: ResHelper
+  ) => InteractionResponse | Promise<InteractionResponse>;
 };
 
 export default function (command: BotCommand) {
